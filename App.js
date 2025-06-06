b01affb4c2507d03d73e444e1efb6420ddd05e79
pavan sai <pavansaipcr@gmail.com>
2025-06-06 15:45:27 +0530

    nested create elements Topic

--- App.js
+++ App.js
@@ -1,3 +1,12 @@
-const ironman=React.createElement("h1",{},"This is react")
-const Jarvis=ReactDOM.createRoot(document.getElementById("Helix"));
-Jarvis.render(ironman);
+//Nested Elements
+
+const parent=React.createElement("div", {id:"parent"}, React.createElement("div", {id:"child"}, React.createElement("h1",{},"I am h1 Tag")));
+console.log(parent);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+
+
+
+//const ironman=React.createElement("h1",{},"This is react")
+//const Jarvis=ReactDOM.createRoot(document.getElementById("Helix"));
+root.render(parent);
