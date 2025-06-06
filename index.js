b7fe633e700bbff9b49ff53c26ff830c6d6f6c61
pavan sai <pavansaipcr@gmail.com>
2025-06-06 13:36:25 +0530

    Overview on react element and create Root

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-const first=React.createElement("h1",{},"Hello I am Example 5")
-            const second=ReactDOM.createRoot(document.getElementById("hello"));
-            second.render(first);
+
+// Here in the {attributes or props} like id, class etc for the props and props are the children and attributes that we pass.
+const first=React.createElement("h1",{id:"heading"},"Hello I am Example 5")  //React.CreateElemnt is a object.
+            const second=ReactDOM.createRoot(document.getElementById("hello")); 
+            second.render(first);  //In the render we are passing the object that is react.createElement
