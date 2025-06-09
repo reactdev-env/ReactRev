ee22f518f04800bda88f8b8188e24ea0c1916c3d
pavan sai <pavansaipcr@gmail.com>
2025-06-09 14:02:58 +0530

    Creating elements old way and new way of react

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
+//Old way of writing create Elemnt in react
 const heading= React.createElement("h1",{id:"react"}, "This is the Object");
 //const root=ReactDOM.createRoot(document.getElementById("root"));
 console.log(heading);
 
+
+//New way of writing react elemnt in react
 const reactJSX=<h1 id="reactJSX">Hello element was created using JSX</h1>
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(reactJSX);
